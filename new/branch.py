b70c7bb55956c0888335f70017207ec71614895a
ASHOKREDDY97 <ashokkumarmvr@gmail.com>
2022-03-24 00:15:56 +0530

    branch

---
+++ new/branch.py
@@ -0,0 +1,32 @@
+# importing os module
+import os
+     
+# Get the current working
+# directory (CWD)
+cwd = os.getcwd()
+     
+# Print the current working
+# directory (CWD)
+print("Current working directory:", cwd)
+
+#Function to Get the current 
+# working directory
+import os
+def current_path():
+    print("Current working directory before")
+    print(os.getcwd())
+    print()
+   
+   
+# # Driver's code
+# # Printing CWD before
+# current_path()
+   
+# # Changing the CWD
+# os.chdir('../')
+   
+# # Printing CWD after
+# current_path()
+
+
+ 
